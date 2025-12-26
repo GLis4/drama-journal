705beaa69b2ff0d1dd995e7f2eba7b7b32c0047e
Glis4 <elisg1711@gmail.com>
2025-12-25 23:31:38 -0300

    fix correct test

--- tests/test_container.py
+++ tests/test_container.py
@@ -2,7 +2,7 @@
 
 class TestMovie:
     def test_not_find_movie(self,client):
-        response = client.get('/api/movie/1')
+        response = client.get('/movie/1')
         print(response.json)
         assert response.status_code == 404
         assert response.json == dict({"error":"Movie {movie_id} not found"})
